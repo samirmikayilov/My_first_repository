bfc7fde32153f6e0272f0611c34a1f08797a1055
Samir Mikayılov <samirmikayilov01@gmail.com>
2024-02-21 00:58:05 +0400

    menim ilk repositorum

---
+++ MY_Git/src/main/java/com/mycompany/my_git/MY_Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.my_git;
+
+/**
+ *
+ * @author Samir muellim
+ */
+public class MY_Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
